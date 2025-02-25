b0b8fa306b3423b7da1fdffbd8bd05181e056a23
Taha Awan <87877306+TahaAwan4206@users.noreply.github.com>
2025-02-24 19:20:43 -0500

    Update page.js

--- src/app/ward-13/page.js
+++ src/app/ward-13/page.js
@@ -3,7 +3,6 @@ import styles from "./page.module.css";
 export default function Ward13() {
     return (
         <>
-     
             <div className={styles.banner}>
                 <img 
                     src="/ward13-banner.png" 
@@ -12,7 +11,6 @@ export default function Ward13() {
                 />
             </div>
 
-         
             <div className={styles.bodyContainer}>
                 <section className={styles.mapsSection}>
                     <h1 className={styles.sectionHeader}>Maps</h1>
@@ -39,16 +37,60 @@ export default function Ward13() {
                                 </div>
                             </details>
                         </li>
+
                         <li>
                             <details>
                                 <summary className={styles.summary}>City of London</summary>
-                                <p>Details about the City of London.</p>
+                                <div style={{ textAlign: "left" }}>
+                                    <ul>
+                                        <li>
+                                            <a 
+                                                href="https://london.maps.arcgis.com/apps/webappviewer/index.html?id=20327d3bcfb34bb488a7c3f74c05d2d3" 
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className={styles.link}
+                                            >
+                                                City of London Zoning Map
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a 
+                                                href="https://london.maps.arcgis.com/apps/webappviewer/index.html?id=0187f8a72f204edcbc95d595f31b5117" 
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className={styles.link}
+                                            >
+                                                City of London Maps
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a 
+                                                href="https://ir.lib.uwo.ca/mdc-London-maps/" 
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className={styles.link}
+                                            >
+                                                London Historical Map Collection 1800-1900
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </details>
                         </li>
+
                         <li>
                             <details>
                                 <summary className={styles.summary}>Property Search</summary>
-                                <p>Details about property search.</p>
+                                <div style={{ textAlign: "left" }}>
+                                    <a
+                                        href="https://bdp.london.ca/citizenportal/app/public-search"
+                                        target="_blank"
+                                        rel="noopener noreferrer"
+                                        className={styles.link}
+                                    >
+                                        City of London Property Inquiry System
+                                    </a>
+                                </div>
                             </details>
                         </li>
                     </ul>
@@ -57,20 +99,67 @@ export default function Ward13() {
                 <section className={styles.resourcesSection}>
                     <h2 className={styles.sectionHeader}>Other Resources</h2>
                     <ul>
+              
                         <li>
                             <details>
                                 <summary className={styles.summary}>
                                     Council and Civic Administration
                                 </summary>
-                                <p>Details about council and civic administration.</p>
+                                <div style={{ textAlign: "left" }}>
+                                    <ul>
+                                        <li>
+                                            <a
+                                                href="https://pub-london.escribemeetings.com/?MeetingViewId=1"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className={styles.link}
+                                            >
+                                                Council and Standing Committee Meetings and Agendas
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a
+                                                href="https://pub-london.escribemeetings.com/?MeetingViewId=2&Year=2025"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className={styles.link}
+                                            >
+                                                Advisory and other Committee Meetings and Agendas
+                                            </a>
+                                        </li>
+                                        <li>
+                                            <a
+                                                href="https://london.ca/government/council-civic-administration"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className={styles.link}
+                                            >
+                                                Quick Links
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </details>
                         </li>
                         <li>
                             <details>
                                 <summary className={styles.summary}>
-                                    Appointment to Council Committees
+                                    City of London Service Portal
                                 </summary>
-                                <p>Details about council and civic administration.</p>
+                                <div style={{ textAlign: "left" }}>
+                                    <ul>
+                                        <li>
+                                            <a
+                                                href="https://service.london.ca/"
+                                                target="_blank"
+                                                rel="noopener noreferrer"
+                                                className={styles.link}
+                                            >
+                                                Service Portal
+                                            </a>
+                                        </li>
+                                    </ul>
+                                </div>
                             </details>
                         </li>
                     </ul>
